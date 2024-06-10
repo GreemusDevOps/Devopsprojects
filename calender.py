0e070553ea24caf74de769105f431277dea38eec
anu <anu@gmail.com>
2024-06-10 15:29:17 +0530

    first commit

--- calender.py
+++ calender.py
@@ -1,4 +1,4 @@
 import calendar    
-yy = int(input("Enter year: "))  
-mm = int(input("Enter month: "))  
+yy=int(input("Enter year:"))  
+mm=int(input("Enter month:"))  
 print(calendar.month(yy,mm))  
